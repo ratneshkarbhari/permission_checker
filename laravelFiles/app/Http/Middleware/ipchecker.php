ff899bbb4fd25b4ff712123ffab90e6d4fe57b46
ratnesh-ultraindia <ratnesh.karbhari@ultraindia.com>
2024-02-13 12:32:31 +0530

    ip restriction back

--- laravelFiles/app/Http/Middleware/ipchecker.php
+++ laravelFiles/app/Http/Middleware/ipchecker.php
@@ -16,10 +16,10 @@ class ipchecker
     public function handle(Request $request, Closure $next): Response
     {
 
-        // if(!in_array($_SERVER['REMOTE_ADDR'],['45.64.206.210','114.143.194.18','::1'])){
-        //     echo "Access not allowed";
-        //     exit;
-        // }
+        if(!in_array($_SERVER['REMOTE_ADDR'],['45.64.206.210','114.143.194.18','::1'])){
+            echo "Access not allowed";
+            exit;
+        }
 
         return $next($request);
     }
